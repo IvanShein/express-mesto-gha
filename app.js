00a59abe7d4b140b8c905640f53e4ff4e88aeda3
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-04-02 17:22:10 +0300

    feat: сoздать схемы и модели пользователя и карточки

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
@@ -9,6 +10,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useCreateIndex: true,
     useFindAndModify: false
 });
-
+app.use(bodyParser.json());
 
 app.listen(PORT);
