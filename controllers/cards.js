20de1aaa6ec01cab99c99079bf0b6735e391f99f
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-04-04 18:00:45 +0300

    fix: исправить обработку ошибок 400, 404, 500

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,7 +4,7 @@ const getAllCards = (req, res) => {
   Cards.find({})
     .then((cards) => res.send(cards))
     .catch((err) => {
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
@@ -13,55 +13,66 @@ const createCard = (req, res) => {
   Cards.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
         return;
       }
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
 const deleteCardById = (req, res) => {
   const { cardId } = req.params;
   Cards.findByIdAndDelete(cardId)
-    .orFail(() => {
-      res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
-    })
     .then((card) => res.send(card))
     .catch((err) => {
-      res.status(500).send(err.message);
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
+        return;
+      }
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
 const putCardLike = (req, res) => {
   const { cardId } = req.params;
-  Cards.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .orFail(() => {
-      res.status(404).send({ message: 'Передан несуществующий_id карточки' });
-    })
+  Cards.findByIdAndUpdate(
+    cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true, runValidators: true },
+  )
     .then((card) => res.send(card))
     .catch((err) => {
+      if (err.name === 'CastError' && err.message.indexOf('_id') >= 0) {
+        res.status(404).send({ message: 'Передан несуществующий_id карточки' });
+        return;
+      }
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
         return;
       }
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
 const deleteCardLike = (req, res) => {
   const { cardId } = req.params;
-  Cards.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .orFail(() => {
-      res.status(404).send({ message: 'Передан несуществующий_id карточки' });
-    })
+  Cards.findByIdAndUpdate(
+    cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true, runValidators: true },
+  )
     .then((card) => res.send(card))
     .catch((err) => {
+      if (err.name === 'CastError' && err.message.indexOf('_id') >= 0) {
+        res.status(404).send({ message: 'Передан несуществующий_id карточки' });
+        return;
+      }
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка' });
         return;
       }
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
--- controllers/users.js
+++ controllers/users.js
@@ -4,19 +4,20 @@ const getAllUsers = (req, res) => {
   Users.find({})
     .then((users) => res.send(users))
     .catch((err) => {
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
 const getUserById = (req, res) => {
   const { userId } = req.params;
   Users.findById(userId)
-    .orFail(() => {
-      res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
-    })
     .then((user) => res.send(user))
     .catch((err) => {
-      res.status(500).send(err.message);
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+        return;
+      }
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
@@ -25,43 +26,45 @@ const createUser = (req, res) => {
   Users.create({ name, about, avatar })
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
         return;
       }
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
-  Users.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-    .orFail(() => {
-      res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
-    })
+  Users.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
+        return;
+      }
+      if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
         return;
       }
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  Users.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .orFail(() => {
-      res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
-    })
+  Users.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
+        return;
+      }
+      if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' });
         return;
       }
-      res.status(500).send(err.message);
+      res.status(500).send(`Ошибка на сервере ${err.name}: ${err.message}`);
     });
 };
 
