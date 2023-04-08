162d19a137ee039a9b9170e5e4a55b7ce2951739
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-04-09 00:39:39 +0300

    feat: создать контроллер login

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Users = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const getAllUsers = (req, res) => {
   Users.find({})
@@ -32,64 +33,86 @@ const getUserById = (req, res) => {
 const createUser = (req, res) => {
   const { name, about, avatar, email, password } = req.body;
   bcrypt.hash(password, 10)
-  .then(hash => {
-  Users.create({ name, about, avatar, email, password: hash })
-    .then((user) => {
-      res.status(201).send({
-        _id: user._id,
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-        email: user.email,
+    .then(hash => {
+      Users.create({ name, about, avatar, email, password: hash })
+        .then((user) => {
+          res.status(201).send({
+            _id: user._id,
+            name: user.name,
+            about: user.about,
+            avatar: user.avatar,
+            email: user.email,
+          })
+        })
+        .catch((err) => {
+          if (err instanceof mongoose.Error.ValidationError) {
+            res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+            return;
+          }
+          res.status(500).send({ message: `Ошибка на сервере ${err.name}: ${err.message}` });
+        });
+    });
+
+  const updateUser = (req, res) => {
+    const { name, about } = req.body;
+    Users.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+      .then((user) => {
+        if (user) {
+          res.send(user);
+        } else {
+          res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+        }
       })
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
-        return;
-      }
-      res.status(500).send({ message: `Ошибка на сервере ${err.name}: ${err.message}` });
-    });
-});
+      .catch((err) => {
+        if (err instanceof mongoose.Error.ValidationError) {
+          res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+          return;
+        }
+        res.status(500).send({ message: `Ошибка на сервере ${err.name}: ${err.message}` });
+      });
+  };
 
-const updateUser = (req, res) => {
-  const { name, about } = req.body;
-  Users.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .then((user) => {
-      if (user) {
-        res.send(user);
-      } else {
-        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
-      }
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-        return;
-      }
-      res.status(500).send({ message: `Ошибка на сервере ${err.name}: ${err.message}` });
-    });
-};
+  const updateAvatar = (req, res) => {
+    const { avatar } = req.body;
+    Users.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+      .then((user) => {
+        if (user) {
+          res.send(user);
+        } else {
+          res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+        }
+      })
+      .catch((err) => {
+        if (err instanceof mongoose.Error.ValidationError) {
+          res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' });
+          return;
+        }
+        res.status(500).send({ message: `Ошибка на сервере ${err.name}: ${err.message}` });
+      });
+  };
 
-const updateAvatar = (req, res) => {
-  const { avatar } = req.body;
-  Users.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => {
-      if (user) {
-        res.send(user);
-      } else {
-        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
-      }
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' });
-        return;
-      }
-      res.status(500).send({ message: `Ошибка на сервере ${err.name}: ${err.message}` });
-    });
-};
+  const login = (req, res) => {
+    const { email, password } = req.body;
 
-module.exports = {
-  getAllUsers, getUserById, createUser, updateUser, updateAvatar,
-};
+    return User.findUserByCredentials(email, password)
+      .then((user) => {
+
+        const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: 7d });
+
+        res.cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true
+        })
+          .end();
+      })
+      .catch((err) => {
+        // ошибка аутентификации
+        res
+          .status(401)
+          .send({ message: err.message });
+      });
+  };
+
+  module.exports = {
+    getAllUsers, getUserById, createUser, updateUser, updateAvatar,
+  };
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -33,5 +34,23 @@ const userSchema = new mongoose.Schema({
     minlength: 8,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
