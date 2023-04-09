6898ace8a493ae19141e15c59833433757cc6fe8
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-04-09 17:14:31 +0300

    feat: защитить авторизацией все маршруты, кроме страницы регистрации и логина

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -27,6 +28,8 @@ mongoose
 
 app.post('/signin', login);
 app.post('/signup', createUser);
+
+app.use(auth);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const auth = (req, res, next) => {
+module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -24,5 +24,3 @@ const auth = (req, res, next) => {
 
   next(); // пропускаем запрос дальше
 };
-
-module.exports = { auth };
